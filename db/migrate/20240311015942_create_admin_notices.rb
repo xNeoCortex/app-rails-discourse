huangchengshun/discourse

DEV: Add new AdminNotice model

db/migrate/20240311015942_create_admin_notices.rb
@@ -1,0 +1,14 @@
+# frozen_string_literal: true
+class CreateAdminNotices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admin_notices do |t|
+      t.integer :category, null: false, index: true
+      t.integer :priority, null: false
+
+      t.string :identifier, null: false, index: true
+      t.json :data, null: false, default: {}
+
+      t.timestamps
+    end
+  end
+end

xNeoCortex/app-rails-discourse

db/migrate/20240311015942_create_admin_notices.rb
@@ -1,0 +1,14 @@
+# frozen_string_literal: true
+class CreateAdminNotices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admin_notices do |t|
+      t.integer :category, null: false, index: true
+      t.integer :priority, null: false
+
+      t.string :identifier, null: false, index: true
+      t.json :details, null: false, default: {}
+
+      t.timestamps
+    end
+  end
+end